abc5f5192ce3b7334c838339c8af215ef2978403
Kevin Pinto <kevinpintocode@gmail.com>
2023-10-18 01:01:15 +0200

    contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,15 +1,39 @@
 import Loader from 'react-loaders';
 import './Contact.scss';
 import AnimatedLetters from '../Animated/AnimatedLetters';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 export default function Contact() {
   const [letterClass, setLetterClass] = useState('text-animate');
+  const form = useRef();
+
   useEffect(() => {
     setTimeout(() => {
       return setLetterClass('text-animate-hover');
     }, 4000);
   }, []);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_8j9cphd',
+        'contact_form',
+        form.current,
+        'dDPJ2T9MK2J4yZkpw'
+      )
+      .then(
+        function (response) {
+          alert('Your message has been successfully sent!');
+          window.location.reload(false);
+        },
+        function (error) {
+          alert('Failed to sent your message, please try again.');
+        }
+      );
+  };
+
   const strArray = 'Contact Me'.split('');
   return (
     <>
@@ -22,6 +46,47 @@ export default function Contact() {
               idx={15}
             />
           </h1>
+          <p>
+            What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
+            printing and typesetting industry. Lorem Ipsum has been the
+            industry's standard dummy text ever since the 1500s
+          </p>
+          <div className="contact-form">
+            <form ref={form} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="Full Name"
+                    required
+                  />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    type="text"
+                    name="subject"
+                    placeholder="Subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea placeholder="Message" name="message"></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
         </div>
       </div>
       <Loader type="pacman" />
